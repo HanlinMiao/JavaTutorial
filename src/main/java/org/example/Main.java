dd745354cad0cde07a2f675b469b141a553e477a
HanlinMiao <hanlin980505@gmail.com>
2025-02-07 15:43:41 -0500

    understanding java foundations

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+// everything is inside a class
+// everything is a member of this class
+// i.e. methods, variables
+public class Main {
+    // Class has methods with identifiers
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    String x = new String("hello"); // property
+    // class - contains everything has members
+    // methods - do something
+    // properties - store something
+    // access modifiers (public, private) - who can use these methods and classes
+    // static - No instance of class is needed
+    // object - instance of a class
+}
