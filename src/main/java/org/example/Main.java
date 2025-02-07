c5ce9f8e4bf0bf73bffb6b492adf2ab81e27893d
HanlinMiao <hanlin980505@gmail.com>
2025-02-07 15:56:04 -0500

    arguments and parameters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,20 @@ package org.example;
 public class Main {
     // Class has methods with identifiers
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello");
+        System.out.println(true);
     }
 
     String x = new String("hello"); // property
-    // class - contains everything has members
-    // methods - do something
-    // properties - store something
-    // access modifiers (public, private) - who can use these methods and classes
-    // static - No instance of class is needed
-    // object - instance of a class
 }
+
+// class - contains everything has members
+// methods - do something
+// arguments - What you pass to a method; (part of the invocation of the method)
+// parameter - variable that stores that data; (part of the definition)
+// properties - store something
+// access modifiers (public, private) - who can use these methods and classes
+// static - No instance of class is needed
+// object - instance of a class
+
+// String[] args is an argument, "Hello world" is an argument
