d45bb5a2f2330e809ec971d9f98ad42301a129d6
HanlinMiao <hanlin980505@gmail.com>
2025-02-18 15:58:18 -0500

    notes on Object-Oriented Programming

---
+++ src/main/java/org/example/Notes.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Notes {
+
+    // Object-Oriented Programming
+    // Classes and Objects
+    // class Person --> blueprint
+    //  name variable : name is a field
+    //  talk() method
+
+    // Object is from instantiating a class
+
+    // Access modifier public vs private
+    // private String name
+    // getter and setter
+    // It will only allow the user to access private attributes through methods
+
+    // methods define some type of behaviors
+}
